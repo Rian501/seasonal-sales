3f28ea311848ce84520e4b3b86a97b7177df5b07
Rian501 <eclaire.lemmon@gmail.com>
2017-06-19 13:44:51 -0500

    Create XHR to fetch products

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,12 +1,31 @@
+console.log("The JS is loaded", );
 let productsRequest = new XMLHttpRequest();
-let categoriesRequest = new XMLHttpeRequest();
+let categoriesRequest = new XMLHttpRequest();
 
-productsRequest.addEventListener("load", );
-categoriesRequest.addEventListener("load", )
+productsRequest.addEventListener("load", displayProducts);
+categoriesRequest.addEventListener("load", displayCategories);
 
 //provide for failure:
 productsRequest.addEventListener("error", executeThisIfXHRFails);
-categories.Request.addEventListener("error", executeThisIfXHRFails);
+categoriesRequest.addEventListener("error", executeThisIfXHRFails);
 function executeThisIfXHRFails() {
   console.log("An error occurred while transferring the data");
 }
+
+
+productsRequest.open("GET", "data/products.json")
+categoriesRequest.open("GET", "data/categories.json")
+
+productsRequest.send();
+categoriesRequest.send();
+
+function displayProducts () {
+	let prodArray = JSON.parse(event.target.responseText).products;
+
+	console.log("products array", prodArray);
+};
+
+function displayCategories () {
+	let categoryArray = JSON.parse(event.target.responseText).categories;
+	console.log("Categories array", categoryArray);
+};
