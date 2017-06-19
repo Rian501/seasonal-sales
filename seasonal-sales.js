a4ba8ece8abecb351956cf212241e3e8f3dfc84d
Rian501 <eclaire.lemmon@gmail.com>
2017-06-19 14:44:40 -0500

    Make HTML dropdown menu

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,27 +1,24 @@
 console.log("The JS is loaded", );
-let productsRequest = new XMLHttpRequest();
-let categoriesRequest = new XMLHttpRequest();
-
-productsRequest.addEventListener("load", displayProducts);
-categoriesRequest.addEventListener("load", displayCategories);
-
-//provide for failure:
-productsRequest.addEventListener("error", executeThisIfXHRFails);
-categoriesRequest.addEventListener("error", executeThisIfXHRFails);
-function executeThisIfXHRFails() {
-  console.log("An error occurred while transferring the data");
-}
 
 
-productsRequest.open("GET", "data/products.json")
-categoriesRequest.open("GET", "data/categories.json")
+(function getProducts() {
+	let productsRequest = new XMLHttpRequest();
+	productsRequest.open("GET", "data/products.json")
+	productsRequest.send();
+	productsRequest.addEventListener("load", displayProducts)
+	productsRequest.addEventListener("error", executeThisIfXHRFails);
+}());
 
-productsRequest.send();
-categoriesRequest.send();
+(function getCategories () {
+	let categoriesRequest = new XMLHttpRequest();
+	categoriesRequest.open("GET", "data/categories.json")
+	categoriesRequest.send();
+	categoriesRequest.addEventListener("load", displayCategories);
+	categoriesRequest.addEventListener("error", executeThisIfXHRFails);
+}());
 
 function displayProducts () {
 	let prodArray = JSON.parse(event.target.responseText).products;
-
 	console.log("products array", prodArray);
 };
 
@@ -29,3 +26,16 @@ function displayCategories () {
 	let categoryArray = JSON.parse(event.target.responseText).categories;
 	console.log("Categories array", categoryArray);
 };
+
+//provide for failure:
+function executeThisIfXHRFails() {
+  console.log("An error occurred while transferring the data");
+}
+
+let outputDiv = document.getElementById('output');
+
+function productCard () {
+	prodArray.forEach
+}
+
+
